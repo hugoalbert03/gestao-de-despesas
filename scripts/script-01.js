c6358c3c83d34ba7512a2f156c25ad858126feb3
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-06 22:56:38 -0300

    Add files via upload

---
+++ scripts/script-01.js
@@ -0,0 +1,31 @@
+let prod = document.querySelector("input.prod");
+let val = document.querySelector("input.val")
+let iteml = document.querySelector("tr.line")
+let itProd = document.querySelector("td.vProd")
+let itVal = document.querySelector("td.vVal")
+
+function read(){
+    if(String(prod.value) == 0 || String(val.value) == 0){
+        window.alert("Preencha o produto e o seu valor completamente!");
+    }else{
+       //itProd.innerHTML =  `${prod.value}`
+       //itVal.innerHTML = `R$ ${Number(val.value).toFixed(2).replace('.',',')}`
+       //Criação da linha da tabela
+       let proxLine = document.createElement("tr")
+       document.querySelector(".tb02").appendChild(proxLine)
+       proxLine.setAttribute("class","cont")
+       //Criação das células da linha
+       let proxContent1 = document.createElement("td")
+       proxLine.appendChild(proxContent1)
+       proxContent1.innerHTML = `${prod.value}`
+       let proxContent2 = document.createElement("td")
+       proxLine.appendChild(proxContent2)
+       proxContent2.innerHTML = `R$ ${Number(val.value).toFixed(2).replace('.',',')}`
+    }
+    
+}
+ 
+function del(){
+    itProd.innerHTML = ""
+    itVal.innerHTML = ""
+}
