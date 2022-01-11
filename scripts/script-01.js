32712e3eafaf72db0b2e1f333b55b31b6f654bc3
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-01-11 18:14:45 -0300

    Limpeza de código e algumas correções

--- scripts/script-01.js
+++ scripts/script-01.js
@@ -1,15 +1,10 @@
 let prod = document.querySelector("input.prod");
 let val = document.querySelector("input.val")
-let iteml = document.querySelector("tr.line")
-let itProd = document.querySelector("td.vProd")
-let itVal = document.querySelector("td.vVal")
 
 function read(){
     if(String(prod.value) == 0 || String(val.value) == 0){
         window.alert("Preencha o produto e o seu valor completamente!");
     }else{
-       //itProd.innerHTML =  `${prod.value}`
-       //itVal.innerHTML = `R$ ${Number(val.value).toFixed(2).replace('.',',')}`
        //Criação da linha da tabela
        let proxLine = document.createElement("tr")
        document.querySelector(".tb02").appendChild(proxLine)
@@ -23,11 +18,10 @@ function read(){
        proxContent2.innerHTML = `R$ ${Number(val.value).toFixed(2).replace('.',',')}`
     }
     prod.value = ""
-    val.value = ""
+    val.value = ""  
     
 }
  
 function del(){
-    itProd.innerHTML = ""
-    itVal.innerHTML = ""
+   
 }
