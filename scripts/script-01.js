6664ebbcd7894726d888fbc82c4a76e962d4f01a
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-02-23 12:49:53 -0300

    Modificação do Visual

--- scripts/script-01.js
+++ scripts/script-01.js
@@ -7,17 +7,18 @@ function read(){
     if(String(prod.value) == 0 || String(val.value) == 0){
         window.alert("Preencha o produto e o seu valor completamente!");
     }else{
-       //Criação da linha da tabela
-       let proxLine = document.createElement("tr")
-       document.querySelector(".tb02").appendChild(proxLine)
-       proxLine.setAttribute("class","cont")
-       //Criação das células da linha
-       let proxContent1 = document.createElement("td")
-       proxLine.appendChild(proxContent1)
-       proxContent1.innerHTML = `${prod.value}`
-       let proxContent2 = document.createElement("td")
-       proxLine.appendChild(proxContent2)
-       proxContent2.innerHTML = `R$ ${Number(val.value).toFixed(2).replace('.',',')}`
+        //Criação da linha da tabela
+        let proxLine = document.createElement("tr")
+        document.querySelector(".tb02").appendChild(proxLine)
+        proxLine.setAttribute("class","cont")
+        //Criação das células da linha
+        let proxContent1 = document.createElement("td")
+        proxContent1.setAttribute("class","nprod")
+        proxLine.appendChild(proxContent1)
+        proxContent1.innerHTML = `${prod.value}`
+        let proxContent2 = document.createElement("td")
+        proxLine.appendChild(proxContent2)
+        proxContent2.innerHTML = `R$ ${Number(val.value).toFixed(2).replace('.',',')}`
     }
     prod.value = ""
     val.value = ""
