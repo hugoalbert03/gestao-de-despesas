efad5e447f14e8ae43a035f06dbad0f72eab0ddb
Hugo Albert <96631538+hugoalbert03@users.noreply.github.com>
2022-04-26 11:39:32 -0300

    Atualização de Interface e Script  26/04 - 01

--- scripts/script-01.js
+++ scripts/script-01.js
@@ -1,11 +1,13 @@
 let prod = document.querySelector("input.prod");
 let val = document.querySelector("input.val")
 var tot = document.querySelector("td.tVal")
+var valM = document.querySelector("#vMonet")
+var vTot = document.querySelector("#vTot")
 let initVal = 0
 function read(){   
     initVal += Number(val.value) 
-    if(String(prod.value) == 0 || String(val.value) == 0){
-        window.alert("Preencha o produto e o seu valor completamente!");
+    if(String(prod.value) == 0 || String(val.value) == 0 || String(valM.value) == 0){
+        window.alert("ATENÇÃO: Preencha todos os campos!");
     }else{
         //Criação da linha da tabela
         let proxLine = document.createElement("tr")
@@ -19,10 +21,14 @@ function read(){
         let proxContent2 = document.createElement("td")
         proxLine.appendChild(proxContent2)
         proxContent2.innerHTML = `R$ ${Number(val.value).toFixed(2).replace('.',',')}`
+
+        prod.value = ""
+        val.value = ""
+        tot.innerHTML = `R$ ${initVal.toFixed(2).replace('.',',')}`
+        var VF = valM.value - initVal
+        vTot.innerHTML = `R$ ${VF.toFixed(2).replace('.',',')}`
     }
-    prod.value = ""
-    val.value = ""
-    tot.innerHTML = `R$ ${initVal.toFixed(2).replace('.',',')}`
+    
 }
 
 function del(){
